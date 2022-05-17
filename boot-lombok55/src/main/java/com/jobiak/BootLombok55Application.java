a9b65f819b2a5d30f420fed9a2a6c340ecbfd0db
saisrujana <saisrujana.nalam@jobiak.com>
2022-05-17 10:42:52 +0530

    Done

---
+++ boot-lombok55/src/main/java/com/jobiak/BootLombok55Application.java
@@ -0,0 +1,13 @@
+package com.jobiak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLombok55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLombok55Application.class, args);
+	}
+
+}
